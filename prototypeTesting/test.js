05db555ccbcc7fd205666d715b37073b861fc099
Alex Adkins <adkins.alexandra@gmail.com>
2014-11-14 10:19:46 -0500

    Very preliminary unrefined final prototype tests.

---
+++ prototypeTesting/test.js
@@ -0,0 +1,8 @@
+function displayWelcome() {
+	// document.getElementById("welcome").innerHTML='<h2>Welcome to Foodback</h2>
+	// 			<h4>Choose what you prefer to be served in the dining hall.<h4>
+	// 			<input type="text" name="username" placeholder="Domain Username"><br>
+	// 			<input type="text" name="password" placeholder="Domain Password"><br>
+	// 			<input type="submit" value="Submit">';
+	document.getElementById("welcome").innerHTML="<h2>Welcome to Foodback</h2><h4>Choose what you prefer to be served in the dining hall.<h4><input type='text' name='username' placeholder='Domain Username'><br>"
+}
